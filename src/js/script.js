2addb1293d0a0b0ba3a058140c3301f611717f97
Eodrigo <rodrigocardosotadeo@gmail.com>
2025-03-28 11:49:30 -0300

    tipos de variaveis

--- src/js/script.js
+++ src/js/script.js
@@ -17,3 +17,18 @@ console.log(exemplo)
 let exemplo1 =null
 console.log(exemplo1)
 
+// tipo de variaveis
+let ex1 = "Fiap";
+console.log(typeof ex1)
+
+let ex2 = 25;
+console.log(typeof ex2)
+
+let ex3 = true;
+console.log(typeof ex3)
+
+let ex4 = {};
+console.log(typeof ex4)
+
+let ex5 = []; 
+console.log(typeof ex5)

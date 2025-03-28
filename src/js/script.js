06f6844ba34b784f801239015695487a9c965b9e
Eodrigo <rodrigocardosotadeo@gmail.com>
2025-03-28 11:42:23 -0300

    declaração de variaveis

---
+++ src/js/script.js
@@ -0,0 +1,19 @@
+// DECLARAÇÃO DE VARIAVEIS
+
+var nome = "Fiap";
+console.log(nome)
+
+let idade =19;
+console.log(idade)
+
+const sobrenome = "Fiapinho";
+console.log(sobrenome)
+
+// undefined
+let exemplo;
+console.log(exemplo)
+
+// nula
+let exemplo1 =null
+console.log(exemplo1)
+
